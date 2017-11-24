de162f88a05fe2c5943c03155baf581c70e74f50
FannarHrafn <fannarhrafn@gmail.com>
2017-11-24 11:56:14 +0000

    klárað login

--- Hlutabref.py
+++ Hlutabref.py
@@ -1,88 +1,133 @@
 #importa allt sem ég nota úr bottle
-from bottle import *
+#  þurfum að setja pymysql upp í pyCharm (sama rútína og bottle-beaker)
 import pymysql
-#routið sem heldur utan um login formið
-@route('/upphafsida')
-#formið fyrir loggin
-def login():
-    return '''
-        <form action="/login" method="post">
-            Notendanafn: <input name="username" type="text" />
-            Lykilorð: <input name="password" type="password" />
-            <input value="Innskrá" type="submit" />
-        </form>
-        <form action="/skra" method="post">
-            Notendanafn: <input name="username" type="text" />
-            Lykilorð: <input name="password" type="password" />
-            <input value="nýskrá" type="submit" />
-        </form>
-            '''
-#route sem tekur við upplýsingum frá loggin forminu
-@route('/login',  method = 'post')
-#Tjekkar hvort notendanafn og lykilorð var rétt
-def do_login():
-    username = request.forms.get('username')
-    password = request.forms.get('password')
-    findinguser = []
-    # tengir okkur við þjón og ákveðinn grunn. cursor object veitir okkur tilvísun á ákveðinn gagnagrunn.
-    conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='1503953219', passwd='mypassword', db='1503953219_vef2verk11')
-    cur = conn.cursor()
-    # lesa alla úr töflunni
-    cur.execute("SELECT skraningarnumer FROM bilar")
-    for row in cur:
-        findinguser.append(row[0])
-    cur.close()
-    conn.close()
-    if username in findinguser:
-        # tengir okkur við þjón og ákveðinn grunn. cursor object veitir okkur tilvísun á ákveðinn gagnagrunn.
-        conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='1503953219', passwd='mypassword',
-                               db='1503953219_vef2verk11')
+from bottle import *
+import datetime
+
+
+#bý til index sem skilar síðu þar sem hægt er að skrá sig inn / skrá nýjan user
+@route('/')
+def index():
+    if request.get_cookie("login") and request.get_cookie("password"):
+        #opna tengingu við sql database
+        conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='2601994049', passwd='mypassword',
+                               db='2601994049_skilaverkefni10')
         cur = conn.cursor()
-        # lesa alla úr töflunni
-        cur.execute("SELECT * FROM bilar WHERE skraningarnumer = %s", (username))
+        #næ í allt úr userbasebase
+        cur.execute("SELECT * FROM userbase")
+        #athuga hverja línu hvort username og password passi með for loop
         for row in cur:
-            user = row
+            #athuga hverja línu hvort username og password passi
+            if row[0]==request.get_cookie("login") and request.get_cookie("password")==row[1]:
+                #loka öllu
+                cur.close()
+                conn.close()
+                #birti síða þar sem notanda tókst að logga inn
+                redirect('/stocks')
+    else:
+        return template('index.tpl')
+
+
+#bý til route fyrir nýja notendur, hvort sem þeim tókst að búa til nýjan user eða ekki
+@route('/newuser',method='POST')
+def newuser():
+    #opna connection í sql database
+    conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='2601994049', passwd='mypassword',
+                           db='2601994049_skilaverkefni10')
+    cur = conn.cursor()
+    #næ í gildi fyrir nýjan user
+    newuser=request.forms.get('newuser')
+    newpass=request.forms.get('newpass')
+    #sql query sem er count fyrir hversu oft newuser kemur fyrir
+    #nota það query í raun til að vita hvort user er þegar til eða ekki
+    cur.execute("SELECT count(*) FROM userbase where username='{}'".format(newuser))
+    #næ í þessa einu línu
+    user = cur.fetchone()
+    #user er lína úr database, athuga fyrsta stak sem er username stakið hvort það er til eða ekki
+    #username stakið er = 1 ef newuser er þegar til því ég bað um count(*) í query
+    if user[0] == 1:
+        #loka öllu
         cur.close()
         conn.close()
-        password = user[1]
-    #ef þau voru rétt, senda þig á leynisíðu og cookies eru verndaðar með leynilykli
-    if username in usernames and  password in passwords:
-        response.set_cookie("account", username, secret='some-secret-key')
-        redirect('/restricted')
-    #Annars birta villuboð
+        #birti síðu
+        return template('newuserno.tpl')
+    #ef user er ekki til þá bý ég hann til
     else:
-        return "<p>Innskráning mistókst rangt notendanafn</p>"
-@route('/skra', method = 'post')
-def skra():
-    # tengir okkur við þjón og ákveðinn grunn. cursor object veitir okkur tilvísun á ákveðinn gagnagrunn.
-    conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='1503953219', passwd='mypassword',
-                           db='1503953219_vef2verk11')
+        #bæti við nýjum user í database
+        cur.execute("INSERT INTO userbase Values('{}','{}')".format(newuser,newpass))
+        conn.commit()
+        #loka öllu
+        cur.close()
+        conn.close()
+        #birti síðu
+        return template('newuseryes.tpl')
+
+
+#bý til login síðu fyrir notendur, hvort sem þeim tókst að loga inn eða ekki
+@route('/login',method='POST')
+def login():
+    #opna tengingu við sql database
+    conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='2601994049', passwd='mypassword',
+                           db='2601994049_skilaverkefni10')
     cur = conn.cursor()
-    t = request.forms.get('tegund')
-    nr = request.forms.get('skraningarnr')
-    cur.execute("Insert into notendur values('{}','{}')".format(nr, t))
-    conn.commit()
+    #sæki upplýsingar
+    username=request.forms.get('username')
+    password=request.forms.get('password')
+    if username=="admin" and password=="tskoli123":
+        redirect('/admin')
+    #næ í allt úr database
+    cur.execute("SELECT * FROM userbase")
+    #athuga hverja línu hvort username og password passi með for loop
+    for row in cur:
+        #athuga hverja línu hvort username og password passi
+        if row[0]==username and row[1]==password:
+            #loka öllu
+            cur.close()
+            conn.close()
+            #bý til tímabil fyrir cookies
+            timabil=datetime.datetime.now() + datetime.timedelta(days=30)
+            #set cookies fyrir login info
+            response.set_cookie("login",row[0],expires=timabil)
+            response.set_cookie("password",row[1],expires=timabil)
+            #birti síða þar sem notanda tókst að logga inn
+            redirect('/stocks')
+    #ef að return var ekki gefið í for loop þá þýðir það að við erum komnir hingað
+    #þá loka ég öllu og birti síðu sem segir notanda að honum tókst ekki að logga inn
     cur.close()
     conn.close()
-    return 'Skráning tókst, vinsamlegast farðu á upphafsíðu og skráðu þig inn!<br><a href="http://localhost:8080/leita">Upphafsíða</a>'
-#leynisíða sem hleypir þér ekki inn nema þú sért með köku sem þú færð við að logga þig inn
-@route('/restricted')
-def restricted_area():
-    username = request.get_cookie("account", secret='some-secret-key')
-    #ef þú ert loggaður inn birta síðu með texta og logout takka
-    if username:
-        return template('''
-        <h1>Velkomin {{name}}.</h1>
-        <form action="/logout" method="get">
-            <input value="Útskrá" type="submit" />
-        </form>
-            ''' , name=username)
-    #Ef þú ert ekki loggaður inn, birta rétt skilaboð
-    else:
-        return "Aðgangur lokaður, Þú ert ekki skráður inn"
+    redirect('/')
+
+
 @route('/logout')
 #eyðir innskráningar cookie og fer með þig aftur á upphafsíðu
 def logout():
-    # Að eyða cookie, yfirskrifum gildið í gildi núll og stillum tíma í fortíðina (mínustala) svo hún rennur út strax.
-    response.set_cookie("account", "", expires=0)
-    redirect('/upphafsida')
+    # Að eyða cookie, yfirskrifum gildið í gildi núll og stillum tíma á 0 svo hún rennur út strax.
+    response.set_cookie("login", "", expires=0)
+    response.set_cookie("password","",expires=0)
+    redirect('/')
+
+
+@route('/stocks')
+def stocks():
+    return template('stocks.tpl')
+
+
+@route('/admin')
+def admin():
+    return template('admin.tpl')
+run()
+@route('/bots')
+def bots():
+    #opna tengingu við sql database
+    conn = pymysql.connect(host='tsuts.tskoli.is', port=3306, user='1503953219', passwd='mypassword',
+                           db='1503953219_skilaverkefni10')
+    cur = conn.cursor()
+    cur.execute("SELECT MAX(UserID) FROM UserBase")
+    nr=cur.fetchall()
+    botsfj=request.forms.get('botsfj')
+    upperrisk=request.forms.get('upperrisk')
+    lowerrisk=request.forms.get('lowerrisk')
+    buyrisk=request.forms.get('buyrisk')
+    #for x in range(botsfj):
+        #y=Bots(upperrisk,lowerrisk,buyrisk,nr)
+        #y.newbot()
